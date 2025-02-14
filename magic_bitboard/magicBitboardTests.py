1b26797c36e5c86ee1201dde9ca3d8e2a16b5e6d
JayO <bluejayvrstudio@gmail.com>
2025-02-14 14:18:29 -0600

    update

--- magic_bitboard/magicBitboardTests.py
+++ magic_bitboard/magicBitboardTests.py
@@ -1,40 +1,5 @@
 import numpy as np
-
-def set_bit(bitboard: np.uint64, square: int) -> np.uint64:
-    return bitboard | (np.uint64(1) << np.uint64(square))
-
-def clear_bit(bitboard: np.uint64, square: int) -> np.uint64:
-    return bitboard & ~(np.uint64(1) << np.uint64(square))
-
-def is_bit_set(bitboard: np.uint64, square: int) -> bool:
-    return (bitboard & (np.uint64(1) << np.uint64(square))) != 0
-
-def print_bitboard(bitboard: np.uint64):
-    for rank in range(7, -1, -1):
-        row = ""
-        for file in range(8):
-            square = rank * 8 + file
-            row += "1 " if is_bit_set(bitboard, square) else ". "
-        print(row)
-    print()
-
-# algebraic to numeric index
-def square_to_index(square: str) -> int:
-    file = ord(square[0]) - ord('a')
-    rank = int(square[1]) - 1
-    return rank * 8 + file
-
-# numeric index to algebraic
-def index_to_square(index: int) -> str:
-    file = index % 8
-    rank = index // 8 + 1
-    return f"{chr(ord('a') + file)}{rank}"
-
-# Constants for board representation
-FILE_A = np.uint64(0x0101010101010101)
-FILE_H = np.uint64(0x8080808080808080)
-RANK_1 = np.uint64(0x00000000000000FF)
-RANK_8 = np.uint64(0xFF00000000000000)
+from bitboard_utilities import *
 
 def example():
     bitboard = np.uint64(0)
